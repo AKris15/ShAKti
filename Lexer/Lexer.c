8e5ad14886729b621e505db78c17c7caba185fe5
AK <herogupta626@gmail.com>
2025-02-23 03:55:37 +0530

    Token Types & Structures

--- Lexer/Lexer.c
+++ Lexer/Lexer.c
@@ -4,3 +4,25 @@
 #include <string.h>
 #include <wchar.h>
 #include <locale.h>
+
+// Token types
+typedef enum {
+    TOKEN_KEYWORD,
+    TOKEN_IDENTIFIER,
+    TOKEN_VARIABLE,
+    TOKEN_CLASSED_VARIABLE,
+    TOKEN_FUNCTION,
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_STRING,
+    TOKEN_COMMENT,
+    TOKEN_SPECIAL_SYMBOL,
+    TOKEN_EOF,
+    TOKEN_UNKNOWN
+} TokenType;
+
+// Token structure
+typedef struct {
+    TokenType type;
+    char value[100];
+} Token;
