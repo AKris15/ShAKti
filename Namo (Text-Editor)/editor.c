2c1b16da2c323529e53a2d45ccc3dcb565f8159e
DISHANT SINGH RATHORE <dishantsinghrathore786@gmail.com>
2025-02-23 18:47:56 +0530

    Update editor.c

--- Namo (Text-Editor)/editor.c
+++ Namo (Text-Editor)/editor.c
@@ -3,14 +3,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <errno.h>
+#include "./editor.h"
 
 #define LINE_INIT_CAPACITY 1024
+#define EDITOR_INIT_CAPACITY 128
 
-typedef struct {
-    char *chars;
-    size_t size;
-    size_t capacity;
-} Line;
+
+static void editor_create_first_new_line(Editor *editor);
+
+
 
 static void line_grow(Line *line, size_t n)
 {
@@ -48,6 +49,7 @@ void line_insert_text_sized_before(Line *line, const char *text, size_t text_siz
         *col = line->size;
     }
 
+
     line_grow(line, text_size);
 
     memmove(line->chars + *col + text_size,
@@ -62,6 +64,7 @@ void line_insert_text_before(Line *line, const char *text, size_t *col)
 {
     line_insert_text_sized_before(line, text, strlen(text), col);
 }
+
 
 void line_backspace(Line *line, size_t *col)
 {
@@ -90,4 +93,102 @@ void line_delete(Line *line, size_t *col)
                 line->size - *col);
         line->size -= 1;
     }
+}
+
+static void editor_grow(Editor *editor, size_t n)
+{
+    size_t new_capacity = editor->capacity;
+
+    assert(new_capacity >= editor->size);
+    while (new_capacity - editor->size < n) {
+        if (new_capacity == 0) {
+            new_capacity = EDITOR_INIT_CAPACITY;
+        } else {
+            new_capacity *= 2;
+        }
+    }
+
+    if (new_capacity != editor->capacity) {
+        editor->lines = realloc(editor->lines, new_capacity * sizeof(editor->lines[0]));
+        editor->capacity = new_capacity;
+    }
+}
+
+void editor_insert_new_line(Editor *editor)
+{
+    if (editor->cursor_row > editor->size) {
+        editor->cursor_row = editor->size;
+    }
+
+    editor_grow(editor, 1);
+
+    const size_t line_size = sizeof(editor->lines[0]);
+    memmove(editor->lines + editor->cursor_row + 1,
+            editor->lines + editor->cursor_row,
+            (editor->size - editor->cursor_row) * line_size);
+    memset(&editor->lines[editor->cursor_row + 1], 0, line_size);
+    editor->cursor_row += 1;
+    editor->cursor_col = 0;
+    editor->size += 1;
+}
+
+
+static void editor_create_first_new_line(Editor *editor)
+{
+    if (editor->cursor_row >= editor->size) {
+        if (editor->size > 0) {
+            editor->cursor_row = editor->size - 1;
+        } else {
+	  editor_grow(editor, 1);
+	  memset(&editor->lines[editor->size], 0, sizeof(editor->lines[0]));
+	  editor->size += 1;
+	  
+	  
+        }
+    }
+}
+
+void editor_insert_text_before_cursor(Editor *editor, const char *text)
+{
+    editor_create_first_new_line(editor);
+    line_insert_text_before(&editor->lines[editor->cursor_row], text, &editor->cursor_col);
+}
+
+void editor_backspace(Editor *editor)
+{
+  editor_create_first_new_line(editor);
+
+  line_backspace(&editor->lines[editor->cursor_row], &editor->cursor_col);
+}
+
+void editor_delete(Editor *editor)
+{
+  editor_create_first_new_line(editor);
+  
+  line_delete(&editor->lines[editor->cursor_row], &editor->cursor_col);
+}
+
+const char *editor_char_under_cursor(const Editor *editor)
+{
+    if (editor->cursor_row < editor->size) {
+        if (editor->cursor_col < editor->lines[editor->cursor_row].size) {
+            return &editor->lines[editor->cursor_row].chars[editor->cursor_col];
+        }
+    }
+    return NULL;
+}
+void editor_save_to_file(const Editor *editor, const char *file_path)
+{
+  FILE *f = fopen (file_path, "w");
+  if (f == NULL) {
+    fprintf(stdout, "ERROR: couldn't open file '%s': %s\n",file_path, strerror(errno));
+    exit(1);
+    
+
+}
+  for (size_t row = 0; row < editor->size; ++row) {
+    fwrite(editor->lines[row].chars, 1, editor->lines[row].size, f);
+    fputc('\n', stdout);
+  }
+  fclose(f);
 }
