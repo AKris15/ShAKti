af049cbb0dd2bea410146ba5611243c13ca648a7
AKris15 <herogupta626@gmail.com>
2025-03-04 18:22:23 +0530

    utils

---
+++ Lexer/utils.c
@@ -0,0 +1,39 @@
+#include "utils.h"
+
+// Keywords in Sanskrit
+const wchar_t *keywords[] = {
+    L"पूर्ण", L"यदि", L"अन्यथा", 
+    L"चक्र", L"से", L"तक", L"लेख", 
+    L"प्रवे", L"कक्ष", L"वा यदि", 
+    L"न", L"कर्म" , NULL
+};
+
+// Boolean literals
+const wchar_t *boolean_literals[] = {
+    L"सत्य", L"असत्य", NULL
+};
+
+// Check if a string is a keyword
+int isKeyword(const wchar_t *word) {
+    for (int i = 0; keywords[i] != NULL; i++) {
+        if (wcscmp(word, keywords[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Check if a string is a boolean literal
+int isBooleanLiteral(const wchar_t *word) {
+    for (int i = 0; boolean_literals[i] != NULL; i++) {
+        if (wcscmp(word, boolean_literals[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Check if a character is a Sanskrit alphabet
+int isSanskritAlpha(wchar_t c) {
+    return (c >= L'\u0900' && c <= L'\u097F') || (c >= L'\uA8E0' && c <= L'\uA8FF');
+}
---
+++ Lexer/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <wchar.h>
+
+// Function prototypes
+int isKeyword(const wchar_t *word);
+int isBooleanLiteral(const wchar_t *word);
+int isSanskritAlpha(wchar_t c);
+
+#endif // UTILS_H
